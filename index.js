01a39468b1f94182e0e7563b5a9908af0382dbaf
Aniket Roy <royaniket2212@gmail.com>
2024-11-02 03:17:54 +0530

    added express support and now showing qr dynamically

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 import * as fs from 'node:fs';
-import inquirer from 'inquirer';
 import qr from "qr-image-color";
+import express from "express";
+import { encode } from 'node:querystring';
 
-inquirer
-  .prompt([{
-    type: "input",
-    name: "acceptURL",
-    message: "Enter any URL: ",
-  }])
-  .then((answers) => {
-    fs.writeFile("URL.txt", answers.acceptURL, (err)=> {
-        if(err) throw err;
-        console.log("URL Accepted!");
-    })
+const port = 8080;
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-    var qr_img = qr.image(answers.acceptURL, {type: "png", color: "#f4c430", background: "black"});
-    qr_img.pipe(fs.createWriteStream("qr_img.png"));
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      throw error;
-    } else {
-      console.log("URL Accepted!");
-    }
-  });
+app.get("/", (req, res)=> {
+  res.render("index.ejs");
+});
+
+app.post("/", (req, res)=> {
+  var qr_img = qr.image(req.body["txt"], {type: "png", color: "black", background: "white"});
+  qr_img.pipe(fs.createWriteStream("./public/images/qr_img.png"));
+  res.render("index.ejs", {foundURL: "images/qr_img.png"});
+});
+
+app.listen(port, ()=> {
+  console.log(`Listening on port ${port}...`);
+});
