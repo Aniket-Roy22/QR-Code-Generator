a92e9daf1cbf9e407ad17e526de00c5fb956fb51
Aniket Roy <royaniket2212@gmail.com>
2024-11-06 21:49:13 +0530

    styling update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import * as fs from 'node:fs';
-import qr from "qr-image-color";
 import express from "express";
+import nodeCanvas from "canvas";
+import QRCodeStyling from "qr-code-styling";
+import { JSDOM } from "jsdom";
+import { image } from 'qr-image-color';
 
 const port = 8080;
 const app = express();
@@ -12,10 +15,52 @@ app.get("/", (req, res)=> {
 });
 
 app.post("/", (req, res)=> {
-  var qr_img = qr.image(req.body["txt"], {type: "png", color: "black", background: "white"});
-  qr_img.pipe(fs.createWriteStream("./public/images/qr_img.png"));
-  
-  res.render("index.ejs", {foundURL: "images/qr_img.png"});
+
+  const options = {
+    width: 300,
+    height: 300,
+    data: req.body["txt"],
+    margin: 7,
+    dotsOptions:
+    { 
+      type: "extra-rounded",
+      gradient:
+      {
+        colorStops: [{ offset: 0, color: "#000" }, { offset: 1, color: "#393E46" }],
+      },
+      roundSize: true,
+    },
+    cornersSquareOptions: {
+      type: "extra-rounded",
+    },
+    cornersDotOptions: {
+      type: "dots",
+      // gradient:
+      // {
+      //   colorStops: [{ offset: 0, color: "#fc4a1a" }, { offset: 1, color: "#f7b733" }],
+      // },
+    },
+    backgroundOptions:
+    {
+      color: "#e9ebee",
+    },
+  }
+
+  const qrCodeImage = new QRCodeStyling({
+    jsdom: JSDOM,
+    nodeCanvas,
+    ...options,
+    imageOptions: {
+        saveAsBlob: true,
+        crossOrigin: "anonymous",
+        margin: 20
+    },
+  });
+
+  qrCodeImage.getRawData("png").then((buffer) => {
+    fs.writeFileSync("./public/images/qr_img.png", buffer);
+    res.render("index.ejs", {foundURL: "images/qr_img.png"});
+  });
 });
 
 app.listen(port, ()=> {
